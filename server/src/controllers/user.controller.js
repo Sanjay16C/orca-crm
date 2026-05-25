5e78bc1fa96093775ce917aabf40fec61b343c91
Sanjay16C <sanjaychandrasekar16@gmail.com>
2026-05-25 12:34:12 +0530

    httponly Cookie added

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -54,7 +54,7 @@ const login = async(req,res) =>{
         res.cookie("refreshToken",refreshToken,{
             httpOnly : true,
             secure : process.env.NODE_ENV === "production",
-            sameSite : "strict",
+            sameSite : process.env.NODE_ENV==="production" ? "none" : "strict",
             maxAge : 7*24*60*60*1000
         });
         res.status(200).json({
@@ -128,7 +128,7 @@ const logout = async(req,res) =>{
         res.clearCookie("refreshToken",{
             httpOnly:true,
             secure: process.env.NODE_ENV === "production",
-            sameSite : "strict"
+            sameSite : process.env.NODE_ENV==="production" ? "none" : "strict"
         });
         res.status(200).json({
             message : "Logout successful"
