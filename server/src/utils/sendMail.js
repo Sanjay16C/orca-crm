af692764bb638634b8a224e63e823cc58b67c418
Sanjay16C <sanjaychandrasekar16@gmail.com>
2026-05-27 13:47:00 +0530

    ipv4 nodemailer change

--- server/src/utils/sendMail.js
+++ server/src/utils/sendMail.js
@@ -5,6 +5,9 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     service : "gmail",
+    family : 4,
+    host: "smtp.gmail.com",
+    port: 465,
     auth : {
         user : process.env.EMAIL_USER,
         pass : process.env.EMAIL_PASS
