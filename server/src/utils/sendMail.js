fc1650421de794982a25961b81ecc4c45f9bef15
Sanjay16C <sanjaychandrasekar16@gmail.com>
2026-05-27 16:06:27 +0530

    change from nodemailer to resend email , due to dns issues

--- server/src/utils/sendMail.js
+++ server/src/utils/sendMail.js
@@ -1,25 +1,15 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 import dotenv from "dotenv";
-import dns from "dns";
 
 dotenv.config();
-dns.setDefaultResultOrder("ipv4first");
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure:true,
-    auth : {
-        user : process.env.EMAIL_USER,
-        pass : process.env.EMAIL_PASS
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000
-});
 
-export const sendMail = async(to,subject,text) =>{
-    await transporter.sendMail({
-        from : process.env.EMAIL_USER,
-        to, subject , text
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendMail = async(to, subject, text) => {
+    await resend.emails.send({
+        from: "onboarding@resend.dev",
+        to,
+        subject,
+        text
     });
 };
