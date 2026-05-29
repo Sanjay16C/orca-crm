b7dc5b6a6472d998fbac88a36b966c82e1bd2dd7
Sanjay16C <sanjaychandrasekar16@gmail.com>
2026-05-29 15:26:10 +0530

    rateLimit added for api brute force protection

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -3,17 +3,20 @@ import { fetchUsers, forgotPassword, googleCallback, login, logout, refreshAcces
 import {authMiddleware} from "../middlewares/auth.middleware.js";
 import { loginValidation, signupValidation } from "../middlewares/validation.middleware.js";
 import passport from "passport";
+import { authLimiter } from "../middlewares/rateLimit.middleware.js";
 
 const userRouter = Router();
 
-userRouter.post("/login",loginValidation,login);
-userRouter.post("/signup",signupValidation,signup);
+userRouter.post("/login",authLimiter,loginValidation,login);
+userRouter.post("/signup",authLimiter,signupValidation,signup);
 userRouter.get("/fetchusers",authMiddleware,fetchUsers);
 userRouter.post("/refresh",refreshAccessToken);
 userRouter.post("/logout",logout);
-userRouter.post("/forgot-password",forgotPassword);
-userRouter.post("/reset-password/:token",resetPassword);
-userRouter.get("/google",passport.authenticate("google",{scope:["profile","email"]}));
+userRouter.post("/forgot-password",authLimiter,forgotPassword);
+userRouter.post("/reset-password/:token",authLimiter,resetPassword);
+userRouter.get("/google",authLimiter,
+    passport.authenticate("google",{scope:["profile","email"]})
+);
 userRouter.get(
     "/google/callback",
     passport.authenticate("google",{session:false,failureRedirect:"/"}),
